81503f6edf4cd609bfdb0e34767e2a14dc620c6d
Me_T <merliti@brightspark.ee>
2019-10-06 13:07:36 +0300

    Varying varables

---
+++ src/com/company/Varying_variables.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Varying_variables {
+    public static void main (String[] args) {
+        int number = 3;
+        double decimal = 5.5;
+        String string = "There is none";
+        // Changes
+        number = 9000;
+        decimal = 0.1;
+        string = "Zetor";
+
+
+        System.out.println("Chickens:");
+        System.out.println(number);
+        System.out.println("Bacon (kg):");
+        System.out.println(decimal);
+        System.out.println("A tracktor");
+        System.out.println(string);
+
+        System.out.println("In a nutshell:");
+        System.out.println(number);
+        System.out.println(decimal);
+        System.out.println(string);
+
+    }
+}
