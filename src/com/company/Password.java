e4b5d3f55ee2394086e5066e79e55f90c6a04eef
Me_T <merliti@brightspark.ee>
2019-10-07 12:19:19 +0300

    If year is a leap year

---
+++ src/com/company/Password.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main (String [] args) {
+        Scanner reader = new Scanner(System.in);
+
+        String password = "dandy";
+
+        while (true){
+            System.out.println("Write the unknown password to reveal a secret:");
+            String passWord = reader.nextLine();
+
+            if (password.equals(passWord)){
+                System.out.println("Well done! The secret is: World is as strange as you want it to be");
+                break;
+            }else{
+                System.out.println("Wrong!");
+            }
+
+        }
+
+
+
+
+
+    }
+}
