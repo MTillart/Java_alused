f1acf71315b6a87f9282091333af8a2697027d28
Me_T <merliti@brightspark.ee>
2019-10-18 11:15:48 +0300

    50 and 55 Type name first three letters and type it reversed

---
+++ src/com/company3/WordInAWord.java
@@ -0,0 +1,27 @@
+package com.company3;
+
+import java.util.Scanner;
+
+public class WordInAWord {
+    public static void main(String [] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Write a  first word: ");
+        String first = reader.nextLine();
+        System.out.println("How about a another word:");
+        String second = reader.nextLine();
+
+        int secLeng = second.length();
+
+        int index = first.indexOf(second);
+        System.out.println(index);
+
+        if (index == -1 ) {
+            System.out.println("The word '" + second + "' is not found in the word '" + first + "'.");
+        }else {
+            System.out.println("The word '" + second + "' is found in the word '" + first + "'.");
+        }
+
+
+
+    }
+}
