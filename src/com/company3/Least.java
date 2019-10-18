29abe21d0835684d7988cbcfcc221ee798210ae4
Me_T <merliti@brightspark.ee>
2019-10-18 10:23:57 +0300

    44 Return smallest number

---
+++ src/com/company3/Least.java
@@ -0,0 +1,19 @@
+package com.company3;
+
+public class Least {
+
+    public static void main(String[] args) {
+        int answer =  least(9, 7);
+        System.out.println("Least: " + answer);
+    }
+    public static int least(int number1, int number2) {
+        // return smallest number
+        if (number1 >  number2) {
+            return number2;
+        }else {
+            return number1;
+        }
+    }
+
+
+}
