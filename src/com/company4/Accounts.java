0c41c9bb74a12119448fbca8c6428e883cb5c0c7
Me_T <merliti@brightspark.ee>
2019-10-22 09:58:12 +0300

    72.3 Money transfer with method

--- src/com/company4/Accounts.java
+++ src/com/company4/Accounts.java
@@ -3,20 +3,25 @@ package com.company4;
 
 public class Accounts {
     public static void main (String [] args) {
-        Account mattAccount = new Account("Matt's account",1000.00);
-        Account myAccount = new Account("My account",0.00);
+        Account aAccount = new Account("A account",100.00);
+        Account bAccount = new Account("B account",0.00);
+        Account cAccount = new Account("C account",0.00);
 
         System.out.println("Initial state");
-        System.out.println(mattAccount);
-        System.out.println(myAccount);
+        System.out.println(aAccount);
+        System.out.println(bAccount);
+        System.out.println(cAccount);
 
-        mattAccount.withdrawal(100);
-        System.out.println("Matt's account balance is now: " + mattAccount.balance());
-        myAccount.deposit(100);
-        System.out.println("My account balance is now: "+myAccount.balance());
+        transfer(aAccount, bAccount, 50);
+        transfer(bAccount, cAccount, 25);
+        System.out.println("A account balance is now: " + aAccount.balance());
+        System.out.println("B account balance is now: "+ bAccount.balance());
+        System.out.println("C account balance is now: "+ cAccount.balance());
 
-        System.out.println("Final state");
-        System.out.println(mattAccount);
-        System.out.println(myAccount);
     }
+    public static void transfer(Account from, Account to, double howMuch){
+        from.withdrawal(howMuch);
+        to.deposit(howMuch);
+    }
+
 }
